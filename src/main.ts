1f6bb3c530838b2bc795a704ef94ae67cc7408c7
adamsir <donuteeze@gmail.com>
2025-05-05 14:54:18 +0200

    basic kickoff

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import './styles/index.css'
+import Lenis from 'lenis';
+
+// Initialize Lenis
+const lenis = new Lenis({
+    autoRaf: true,
+});
+
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+    console.log(e);
+});
